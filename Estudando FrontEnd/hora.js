8fd31ca403da2e3cc271d21224441a26b24b2970
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-11 18:48:33 -0300

    Create hora.js

---
+++ Estudando FrontEnd/hora.js
@@ -0,0 +1,9 @@
+var agora = new Date()
+var hora = agora.getHours()
+var minutos = agora.getMinutes()
+console.log(`${hora}:${minutos}`)
+
+var dia = agora.getDay()
+var mes = agora.getMonth()
+var ano = agora.getFullYear()
+console.log(`${dia}/${mes}/${ano}`)
