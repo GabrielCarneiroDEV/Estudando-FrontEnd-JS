508d1e3c26434cb2a90b07b6244d97b641717968
Gabriel Carneiro <gabriel29music@gmail.com>
2020-12-15 23:40:15 -0300

    Update script5.js

--- Estudando FrontEnd/exercicios/exercício 5/script5.js
+++ Estudando FrontEnd/exercicios/exercício 5/script5.js
@@ -7,30 +7,58 @@
 
 
 //------------------------------------------------------
+let selAnalisador = document.getElementById('selAnalisador')
+let adds = document.getElementById('txtnum')
+
+let res = document.getElementById('res')
+let valores =[]
+let adicionados
+
+
 
 
 
 function adicionar(){
- //let res = document.getElementById('res')
-    let selAnalisador = document.getElementById('selAnalisador')
-    let adds = document.getElementById('txtnum')
-    let adicionados = Number(adds.value)
-    let res = document.getElementById('res')
-    
-    
-    let item = document.createElement('option')
-    item.text = `${adicionados}`
-    selAnalisador.appendChild(item)
-    let valores =[]
-    valores.push(adicionados)
-    
+ 
+ let item = document.createElement('option')
 
+    
+if (adds.value.length == 0 || adds.value > 100 ){
+
+    window.alert("ERRO - VALOR INCORRETO")
+} else{
+
+    adicionados =Number(adds.value)
+    valores.push(adicionados)
+    selAnalisador.appendChild(item)
+     item.text = `${adicionados}`
+   
+   
+}
  
 }
 
 function analisar(){
+    let soma = 0;
+    let maiorNum=0
+    let nums = 0 
+    let media = 0
+    for (let i = 0; i < valores.length; i++){
+
+       soma += valores[i];
+       
+       nums = valores[i]
+       if(maiorNum < valores[i]){
+           maiorNum = valores[i]
+       }
+
 
    
-    res.innerHTML = `${valores}`
+
+    }
+   
+    media = soma / valores.length
+    res.innerHTML = `${valores} <br> a soma é ${soma} <br> o maior numero é ${maiorNum} <br> a média é ${media.toFixed(2)}`
+    
     
     }
